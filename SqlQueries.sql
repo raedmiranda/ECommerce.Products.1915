045fca4335376f33b90d5edafd99026afe4ebf2c
raedmiranda <raul.miranda95@outlook.com>
2022-04-18 13:15:33 -0500

    queries

---
+++ SqlQueries.sql
@@ -0,0 +1,36 @@
+﻿create procedure spListarProductos
+-- @Id int
+as
+begin
+	select * 
+	from Producto
+	where Estado = 1;
+end;
+
+GO;
+
+-- EXEC spListarProductos @Id = 1;
+
+
+create procedure spInsertarProducto
+	@Nombre varchar(100),
+	@Descripcion varchar(300),
+	@Precio decimal(6,2)
+as
+begin
+	insert into Producto(Nombre, Descripcion, Precio) 
+	values (@Nombre, @Descripcion, @Precio) 
+end;
+
+
+
+
+
+create procedure spObtenerProductos
+	@Id int
+as
+begin
+	select * 
+	from Producto
+	where Estado = 1 and Id = @Id;
+end;
